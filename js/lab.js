79b1e65a28aaa8b8ac68b28b1a1ab176fb8063ce
Nicolas FABRI <fabri.nicolas@yahoo.fr>
2015-05-10 21:29:53 +0200

    recup des resultats necessaires sur l'api github

--- js/lab.js
+++ js/lab.js
@@ -1,27 +1,55 @@
-    google.load("feeds", "1");
-
-    function initialize() {
-      var feed = new google.feeds.Feed("https://github.com/Squareseidh.atom");
-      feed.load(function(result) {
-        console.log(result);
-      });
-    }
-    google.setOnLoadCallback(initialize);
-
-
 /*global  $, window*/
 function principale() {
     "use strict";
+    var i;
     
     $.ajax({
         url: 'https://api.github.com/users/Squareseidh',
         success: function (resultat) {
-                   console.log(resultat);
+            var avatar = $("<img src='" + resultat.avatar_url + "' alt='" + resultat.login + "'>"),
+                lienGithub = $("<a href='" + resultat.html_url + "' target='_blank'></a>"),
+                username = $("<h1>" + resultat.login + "</h1>"),
+                followers = $("<h2>" + resultat.followers + " followers</h2>"),
+                following = $("<h2>" + resultat.following + " following</h2>"),
+                location = $("<h2>" + resultat.location + "</h2>");
+            
+            lienGithub.append(avatar);
+            
+            $('#githubUser').append(lienGithub);
+            $('#githubUser').append(username);
+            $('#githubUser').append(followers);
+            $('#githubUser').append(following);
+            $('#githubUser').append(location);
         }
     });
     
     $.ajax({
 	    url: 'https://api.github.com/users/Squareseidh/repos',
+        success: function (resultat) {
+            console.log(resultat);
+            for (i = 0; i < resultat.length; i += 1) {
+                
+                var nom = $("<h1>" + resultat[i].name + "</h1>"),
+                    lienRepo = $("<a href='" + resultat[i].git_url + "' target='_blank'></a>"),
+                    descr = $("<p>" + resultat[i].description + "</p>"),
+                    creation = $("<h3>Crée le: " + resultat[i].created_at + "</h3>"),
+                    language = $("<h2>" + resultat[i].language + "</h2>"),
+                    repo = $('<section></section>');
+            
+                lienRepo.append(nom);
+                repo.append(nom);
+                repo.append(descr);
+                repo.append(creation);
+                repo.append(language);
+                console.log(repo);
+                
+                $('#githubRepo').append(repo);
+            }
+        }
+    });
+    
+    $.ajax({
+	    url: 'https://api.github.com/users/Squareseidh/events',
         success: function (resultat) {
             console.log(resultat);
         }
